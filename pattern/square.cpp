7cc6150aeb0f331d69ee24371d2e035b5103bc86
ShampaShaw <shampashaw088@gmail.com>
2023-07-16 09:36:26 +0530

    3rd commit

--- pattern/square.cpp
+++ pattern/square.cpp
@@ -159,13 +159,120 @@ void print15(int n){
 }
 
 void print16(int n){
-    char ch='A';
+    
     for(int i=0; i<n; i++){
+        char ch='A'+i;
         for( int j=0; j<=i; j++){
             cout<< ch;
         }
         cout << endl;
-        ch+= i;
+    }
+}
+
+void print17(int n){
+    for(int i=0; i<n; i++){
+       for(int j=0; j<n-i-1; j++){
+        cout<<" ";
+       }
+       char ch='A';
+       int b = (2*i+1)/2;
+       for(int j=1; j<= 2*i+1; j++){
+        cout << ch;
+        if(j<=b) ch++;
+        else ch--;
+       }
+
+       for(int j=0; j<n-i-1; j++){
+        cout<<" ";
+       }
+        cout<< endl;
+    }
+}
+
+void print18(int n){
+    for(int i=0; i<n; i++){
+        char ch='E'-i;
+        for(int j=0 ; j<=i; j++){
+            cout << ch;
+        }
+        cout<< endl;
+    }
+}
+
+void print191(int n){
+    for(int i=0; i<n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<< "*";
+        }
+        for(int j=1; j<= 2*i; j++){
+            cout<<" ";
+        }
+        for(int j=n-i; j>=1; j--){
+            cout<< "*";
+        }
+        cout<< endl;
+    }
+}
+
+void print192(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<"*";
+        }
+        for(int j=0 ; j<= 2*(n-i)-2; j++) {
+            cout<< " ";
+        }
+         for(int j=0; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print20(int n){
+    int space = 2*n-2;
+    for(int i=1; i<=2*n-1; i++){
+        int s= i;
+        if(i>n) s=2*n-i;
+        for(int j=1; j<=s; j++){
+            cout<< "*";
+        }
+
+        for(int j=1; j<=space; j++){
+            cout<<" ";
+        }
+
+        for(int j=1; j<=s; j++){
+            cout<< "*";
+        }
+        cout<<endl;
+        if(i<n) space-=2;
+        else space+=2;
+    }
+}
+
+void print21(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0 ; j<n; j++){
+            if(j==0 || j==n-1 || i==n-1 || i==0){
+                cout<< "*";
+            }
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void print22(int n){
+    for(int i=0; i<2*n-1; i++){
+        for(int j=0; j<2*n-1; j++){
+           int top=i;
+           int left= j;
+           int right= (2*n-2)-j;
+           int bottom = (2*n-2)-i;
+           cout<< (n- min(min(top,bottom), min(left,right)));
+        }
+         cout<<endl;
     }
 }
 
@@ -190,4 +297,10 @@ int main() {
     print14(n);
     print15(n);
     print16(n);
+    print17(n);
+     print18(n);
+     print191(n);
+     print192(n);
+     print20(n);
+     print21(n);
 }
