b308200690971808743ab4692a9a3a8f450f8407
ShampaShaw <shampashaw088@gmail.com>
2023-07-16 11:20:51 +0530

    List.cpp

---
+++ STL/List.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainList(){
+    list<int> ls;
+
+    ls.push_back(2); //{2}
+    ls.emplace_back(4); //{2,4}
+
+    //insert function take lot of time
+
+    ls.push_front(5); //{5,2,4}
+    ls.emplace_front(); //{2,4};
+
+    //rest function same as vector
+}
